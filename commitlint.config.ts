3ba08dd2058de875c4b7d4faa8058c77a5bdf9b6
Muneeb Mughal <muneebmughal3324@gmail.com>
2025-01-05 14:35:35 +0500

    [develop] chore:config commitlint

---
+++ commitlint.config.ts
@@ -0,0 +1,15 @@
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "docs", "style", "refactor", "test", "chore", "ci"],
+    ],
+    "type-case": [0],
+    "scope-case": [0],
+    "subject-empty": [0],
+    "type-empty": [0],
+    "subject-case": [0],
+  },
+};
