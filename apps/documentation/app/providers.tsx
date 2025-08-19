48e3603002024518783f1b9ee5fc9e4b1dd6326d
Muneeb Mughal <muneeb.it.dev@gmail.com>
2025-08-19 15:45:57 +0500

    docs:complete initial configrations

---
+++ apps/documentation/app/providers.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { FlexiUIProvider } from '@flexi-ui/system'
+import { type ThemeProviderProps, ThemeProvider as NextThemesProvider } from 'next-themes'
+import { useRouter } from 'next/navigation'
+
+export interface ProvidersProps {
+  children: React.ReactNode
+  themeProps?: ThemeProviderProps
+}
+
+export const Providers = ({ children, themeProps }: ProvidersProps) => {
+  const router = useRouter()
+  return (
+    <FlexiUIProvider navigate={router.push}>
+      <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+    </FlexiUIProvider>
+  )
+}
